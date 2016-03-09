2ecf8fffdf93752cea89e2583ae6e89654452e6a
mttcrsp <mttcrsp@gmail.com>
2016-03-09 19:50:45 +0100

    Fix Role initialization

--- src/Role.js
+++ src/Role.js
@@ -1,30 +1,15 @@
-const Pattern = require('./Pattern');
+// FIXME: Add parameters type checking
 
 const Permission = (operation, pattern) => {
-    if (pattern.prototype === 'Pattern') {
-        throw new TypeError('Expected pattern to be a Pattern object.');
-    }
     return { operation, pattern };
 };
 
-const Role = (superroles, permissions) => {
-    if (superroles.every(role => role.prototype === 'role')) {
-        throw new TypeError('Expected superroles to be an array of Role ' +
-        'objects.');
-    }
-    return [
+const Role = (permissions, _superroles) => {
+    const superroles = _superroles || [];
+    return () => [
         ...superroles.map(role => role()),
         ...permissions
     ];
 };
 
-Array.prototype.every = function (predicate) {
-    for (let i = 0; i < this.length; i++) {
-        if (!predicate(this[i])) {
-            return false;
-        }
-    }
-    return true;
-};
-
-module.exports = Role;
+exports = { Permission, Role };
