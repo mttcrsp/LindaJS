08cd8f83a6d2ea74d7d59bd84dbe224a21cd5b82
mttcrsp <mttcrsp@gmail.com>
2016-03-10 15:31:15 +0100

    Add operation identifiers

--- src/Role.js
+++ src/Role.js
@@ -1,7 +1,21 @@
+'use strict';
+
 // FIXME: Add parameters type checking
 
-const Permission = (operation, pattern) => {
-    return { operation, pattern };
+const Permission = (type, pattern) => {
+    const types = Object.keys(Permission.TYPES);
+    for (let i = 0; i < types.length; i++) {
+        if (type === types[i]) {
+            return { type, pattern };
+        }
+    }
+    throw new TypeError('Expected operationType to be a valid operation type.');
+};
+
+Permission.TYPES = {
+    IN: 'IN',
+    OUT: 'OUT',
+    EVAL: 'EVAL'
 };
 
 const Role = (permissions, _superroles) => {
