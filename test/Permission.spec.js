c9b47d2e73a8a9215edf35ad6759d8df1e0dccb9
mttcrsp <mttcrsp@gmail.com>
2016-03-11 11:42:43 +0100

    Add Role and Permission unit tests

---
+++ test/Permission.spec.js
@@ -0,0 +1,63 @@
+/*eslint-disable no-undef,max-nested-callbacks*/
+
+'use strict';
+
+const expect = require('expect');
+
+const Pattern = require('../src/Pattern');
+const Permission = require('../src/Permission');
+const Operation = require('../src/Operation');
+
+describe('Permission', function () {
+    describe('constructor', function () {
+        it('should thrown an exception if an invalid invalid parameter was provided', function () {
+            expect(() => {
+                Permission('something', [1]);
+            }).toThrow();
+        });
+    });
+
+    describe('#authorizes', function () {
+        it('should return true for compatible operations', function () {
+            const write = Operation(Operation.TYPE.OUT, [1]);
+            const permission = Permission(
+                Operation.TYPE.OUT,
+                Pattern(1)
+            );
+            expect(
+                permission.authorizes(write)
+            ).toBe(true);
+        });
+
+        it('should return false for operations with an incompatible type', function () {
+            const write = Operation(Operation.TYPE.OUT, [1]);
+            const permission = Permission(
+                Operation.TYPE.IN,
+                Pattern(1)
+            );
+            expect(
+                permission.authorizes(write)
+            ).toBe(false);
+        });
+
+        it('should return false for operation with an incompatible operand', function () {
+            const read = Operation(Operation.TYPE.IN, Pattern(1));
+            const readPermission = Permission(
+                Operation.TYPE.IN,
+                Pattern(2)
+            );
+            expect(
+                readPermission.authorizes(read)
+            ).toBe(false);
+
+            const write = Operation(Operation.TYPE.OUT, [1]);
+            const writePermission = Permission(
+                Operation.TYPE.OUT,
+                Pattern(2)
+            );
+            expect(
+                writePermission.authorizes(write)
+            ).toBe(false);
+        });
+    });
+});
---
+++ test/Role.spec.js
@@ -0,0 +1,80 @@
+/*eslint-disable no-undef,max-nested-callbacks*/
+
+'use strict';
+
+const expect = require('expect');
+
+const Pattern = require('../src/Pattern');
+const Permission = require('../src/Permission');
+const Operation = require('../src/Operation');
+const Role = require('../src/Role');
+
+describe('Role', function () {
+    describe('#constructor', function () {
+        it('should return a role with the specified permissions', function () {
+            const permissions = [
+                Permission(Operation.TYPE.OUT, Pattern(1)),
+                Permission(Operation.TYPE.IN, Pattern(1))
+            ];
+
+            const role = Role(permissions);
+            expect(
+                role.getPermissions().length
+            ).toBe(2);
+        });
+
+        it('should return a role that inherits permissions from the specified superroles', function () {
+            const permissions = [
+                Permission(Operation.TYPE.OUT, Pattern(1)),
+                Permission(Operation.TYPE.IN, Pattern(1))
+            ];
+
+            const parentRole = Role(permissions);
+
+            const newPermission = Permission(
+                Operation.TYPE.EVAL,
+                Pattern(1)
+            );
+            const role = Role([newPermission], [parentRole]);
+            expect(
+                role.getPermissions().length
+            ).toBe(3);
+        });
+
+        it('should return a role that inherits permissions from  multiple parents', function () {
+            const write = Permission(Operation.TYPE.OUT, Pattern(1));
+            const read = Permission(Operation.TYPE.IN, Pattern(1));
+
+            const parent1 = Role([write]);
+            const parent2 = Role([read]);
+
+            const role = Role([], [parent1, parent2]);
+            expect(
+                role.getPermissions().length
+            ).toBe(2);
+        });
+    });
+
+    describe('#can(operation)', function () {
+        it('should return true if the role has a compatible permission', function () {
+            const operation = Operation(Operation.TYPE.OUT, [1]);
+            const read = Permission(Operation.TYPE.IN, Pattern(1));
+            const write = Permission(Operation.TYPE.OUT, Pattern(1));
+            const role = Role([read, write]);
+
+            expect(
+                role.can(operation)
+            ).toBe(true);
+        });
+
+        it('should return false if the role does have a compatible permission', function () {
+            const operation = Operation(Operation.TYPE.OUT, [1]);
+            const read = Permission(Operation.TYPE.IN, Pattern(1));
+            const role = Role([read]);
+
+            expect(
+                role.can(operation)
+            ).toBe(false);
+        });
+    });
+});
