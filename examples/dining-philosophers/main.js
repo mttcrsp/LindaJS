6220ff58480672ee97d05959ab50c224bb363a39
mttcrsp <mttcrsp@gmail.com>
2016-07-11 14:57:32 +0200

    Improve dining philosophers example

--- examples/dining-philosophers/main.js
+++ examples/dining-philosophers/main.js
@@ -1,6 +1,3 @@
-const async = require('async')
-const Range = require('lodash').range
-
 // This examples shows the basic API of this library. The interesting bit is
 // represented by the fact that a philosopher only has to know about how to
 // implement its behavior and then is able to live inside the space. This
@@ -8,83 +5,113 @@ const Range = require('lodash').range
 // at its best since deadlock situations may arise; as a matter of facts this
 // program alts when a deadlock is reached.
 const Linda = require('../../src/Linda')
-const Store = Linda.Store
-const Space = Linda.Space
+const Range = require('lodash').range
+const async = require('async')
+const whilst = async.whilst
+const series = async.series
+const apply = async.apply
+
+const FOREVER = () => true
+
+const State = {
+  THINK: 'THINK',
+  PREPARE: 'PREPARE',
+  EAT: 'EAT',
+  RELEASE: 'RELEASE'
+}
+
+const random = (min, max) => {
+  return Math.floor(Math.random() * max) + min
+}
+
+// This variable controls the number of philosophers that will be generated
+const PHILOSOPHERS_COUNT = 5
 
 // These next few lines setup the space with the necessary tuples: the forks
 // and tickets that philosophers will use to eat.
-
-// This variable controls the number of philosophers that will be generated
-const PHILOSOPHERS_COUNT = 3
-
-const tickets = Range(2).map(() => {
-  return { type: 'room ticket' }
-})
-const forks = Range(PHILOSOPHERS_COUNT + 1).map(i => {
+const forks = Range(PHILOSOPHERS_COUNT).map(i => {
   return { type: 'fork', index: i }
 })
-
-const store = Store([
-  ...tickets,
-  ...forks
-])
-const space = Space(store)
+const store = Linda.Store(forks)
+const space = Linda.Space(store)
 
 // A philosopher is a simple evolution of an agent. It encapsulates an agent
 // and implement 4 basic behaviors:
-//  1. Think for some amount of time
-//  2. When you are done with thinking start looking for the resources you need
-//     in this order: ticket, left fork, right fork
+//  1. Think for some amount of time (THINK STATE)
+//  2. When you are done with thinking start looking for the resources you
+//     need: your left fork and your right fork (PREPARE STATE)
 //  3. When you have acquired all of the necessary resources start eating for
-//     an indefinite amount of time
+//     an indefinite amount of time (EAT STATE)
 //  4. When you are done eating return the resources you previously acquired.
+//     (PREPARE STATE)
 const Philosopher = id => {
   const agent = space.createAgent()
 
   const left = id
-  const right = (id + 1) % 3
+  const right = (id + 1) % PHILOSOPHERS_COUNT
+
+  let state = State.PREPARE
 
   const eat = cb => {
+    state = State.EAT
     setTimeout(() => {
-      async.series([
-        async.apply(agent.take, { type: 'room ticket' }),
-        async.apply(agent.take, { type: 'fork', index: left }),
-        async.apply(agent.take, { type: 'fork', index: right })
-      ], () => {
-        console.log(id + ' is thinking')
-        cb()
-      })
-    }, Math.floor(Math.random() * 1000) + 500)
+      state = State.RELEASE
+      cb()
+    }, random(500, 1000))
   }
 
   const think = cb => {
+    state = State.THINK
     setTimeout(() => {
-      async.series([
-        async.apply(agent.write, { type: 'room ticket' }),
-        async.apply(agent.write, { type: 'fork', index: left }),
-        async.apply(agent.write, { type: 'fork', index: right })
-      ], () => {
-        console.log(id + ' is eating')
-        cb()
-      })
-    }, Math.floor(Math.random() * 3000) + 500)
+      state = State.PREPARE
+      cb()
+    }, random(500, 3000))
   }
 
   return {
+    getState () {
+      switch (state) {
+        case State.THINK:
+          return 'T'
+        case State.EAT:
+          return 'E'
+        case State.PREPARE:
+          return 'P'
+        case State.RELEASE:
+          return 'R'
+      }
+    },
     live () {
-      async.whilst(() => true, cb => {
-        async.series([eat, think], cb)
+      whilst(FOREVER, cb => {
+        series([
+          apply(agent.take, { type: 'fork', index: left }),
+          apply(agent.take, { type: 'fork', index: right }),
+          eat,
+          apply(agent.write, { type: 'fork', index: right }),
+          apply(agent.write, { type: 'fork', index: left }),
+          think
+        ], cb)
       })
     }
   }
 }
 
-// Instantiate the philosopers
-const philosophers = Range(PHILOSOPHERS_COUNT).map(
-  i => Philosopher(i)
-)
-
+// Instantiate the philosophers
+const philosophers = Range(PHILOSOPHERS_COUNT).map(Philosopher)
 // Start the interaction
 philosophers.forEach(
-  philosopher => philosopher.live()
+  philosopher => setTimeout(() => philosopher.live(), random(500, 5000))
 )
+
+// Log the overall state changes to the console
+const getPhilosophersDescription = philosophers => philosophers.reduce(
+  (previous, philosopher) => {
+    const philosopherState = philosopher.getState()
+    return `${previous} ${philosopherState} `
+  },
+  ''
+)
+setInterval(() => {
+  const systemState = getPhilosophersDescription(philosophers)
+  console.log(systemState)
+}, 500)
